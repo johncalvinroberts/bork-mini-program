e173307a85709e59f6262892d675b20bea74e9c4
John Roberts <john.calvin.roberts@gmail.com>
2018-01-25 17:34:44 +0800

    locales for componeent

--- src/mixins/localesmixin.js
+++ src/mixins/localesmixin.js
@@ -3,7 +3,6 @@ import wepy from 'wepy'
 export default class LocalesMixin extends wepy.mixin {
   constructor () {
     super()
-    console.log('locales constructor')
     const isWeb = wepy.env === 'web'
     if (isWeb) {
       this.handleWebLocales()
@@ -25,16 +24,13 @@ export default class LocalesMixin extends wepy.mixin {
   }
 
   onLoad () {
-    console.log('mixin onload')
     const pages = getCurrentPages()[0] // eslint-disable-line
     this.currentPage = pages ? pages.route.split('pages/')[1] : 'landing'
-    console.log(this.currentPage)
     this.$apply()
     this.setLocales()
   }
 
   handleWebLocales () {
-    console.log('handleWebLocales')
     this.currentPage = window.location.hash.split('#!/pages/')[1]
     const _ = wepy.T._
     const pageLocales = _(this.currentPage)
--- src/utils/locales.js
+++ src/utils/locales.js
@@ -6,10 +6,7 @@ export default {
       "view_dogs" : "view dogs",
       "view_cats" : "view cats",
       "cta_rescuer": "I'm a rescuer",
-      "cta_adopter": "I want to adopt",
-      "login": "Login",
-      "modal_title":"U need to register first😁",
-      "modal_subtitle": "You can sign up to be an adopter or rescuer"
+      "login": "Login"
     }
   },
   "zh_CN": {
@@ -17,10 +14,7 @@ export default {
       "view_dogs": "看狗狗",
       "view_cats" : "看猫猫",
       "cta_rescuer": "我是救狗救猫的人",
-      "cta_adopter": "我想领养",
-      "login": "登录",
-      "modal_title":"你还没有注册😁",
-      "modal_subtitle": "你可以成为adopter还是rescuer"
+      "login": "登录"
     }
   }
 }
