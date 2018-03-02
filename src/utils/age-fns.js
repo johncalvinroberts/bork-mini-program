adaa538f690053048339f70467da2e26be1b922c
John Roberts <john.calvin.roberts@gmail.com>
2018-03-02 15:58:38 +0800

    swipe to close sidebar; commit before replacing css w animation

--- src/utils/age-fns.js
+++ src/utils/age-fns.js
@@ -29,12 +29,15 @@ export const _unitAndDaysToNumber = (unit, days) => {
 
 export const _daysToString = (days) => {
   const locale = (wepy.T.locale === 'en') ? locales['en'] : locales['zh_CN']
-  if (days < 50) {
+  if (days <= 50) {
     const weeks = Math.floor(parseInt(days) / 7)
     return `${weeks} ${locale['weeks']}`
-  } else if (days > 50 && days < 365) {
+  } else if (days > 50 && days <= 365) {
     const months = Math.floor(parseInt(days) / 30)
     return `${months} ${locale['months']}`
+  } else if (days > 365 && days <= 1460) {
+    const years = (parseInt(days) / 365).toFixed(1)
+    return `${years} ${locale['years']}`
   } else {
     const years = Math.floor(parseInt(days) / 365)
     return `${years} ${locale['years']}`
@@ -43,6 +46,6 @@ export const _daysToString = (days) => {
 
 export const _getUnit = (days) => {
   if (days < 50) return 'weeks'
-  if (days > 50 && days < 365) return 'months'
-  if (days > 365) return 'years'
+  if (days >= 50 && days < 365) return 'months'
+  if (days >= 365) return 'years'
 }
